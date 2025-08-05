0e812386415b3f1f93e9d4a0f56798187674e5d2
teddav <teddav@users.noreply.github.com>
2025-08-06 00:15:12 +0200

    add details for projects

--- src/projects.tsx
+++ src/projects.tsx
@@ -53,6 +53,20 @@ const work: Record<string, TProject[]> = {
       href: "https://github.com/teddav/zk-tenant",
       tags: [Tags.privacy, Tags.zk, Tags.noir],
       startDate: new Date("2025-03-20"),
+      details: (
+        <>
+          Built a PoC for French people to avoid sharing their ID documents with their landlords.
+          <br />
+          Takes in an ID and a salary slip, and outputs a ZK proof that the ID is valid, and the salary is enough to cover the rent.
+          <br />
+          See the detailed post{" "}
+          <ExternalLink href="https://blog.hyli.org/privacy-preserving-housing-applications-with-david/">on Hyli&apos;s blog</ExternalLink>
+          <br />
+          You can play with the Noir circuits on <ExternalLink href="https://github.com/teddav/tdd.nr">tdd.nr</ExternalLink> repo.
+          <br />
+          If you&apos;re lucky, maybe the app <ExternalLink href="https://zk-tenant.vercel.app/">is still live...</ExternalLink>
+        </>
+      ),
     },
     {
       title: "co-match",
@@ -60,10 +74,21 @@ const work: Record<string, TProject[]> = {
       href: "https://github.com/teddav/co-match.noir",
       tags: [Tags.mpc, Tags.co_snarks, Tags.taceo],
       startDate: new Date("2025-05-05"),
+      details: (
+        <>
+          I built a Private dating app 🌶️ using TACEO&apos;s co-noir (experimental) tech.
+          <br />
+          It was a fun project to see what&apos;s possible with{" "}
+          <ExternalLink href="https://core.taceo.io/articles/private-shared-state/">private shared state</ExternalLink>
+          <br />
+          App is probably down, but just in case if you want to take a look:{" "}
+          <ExternalLink href="https://co-match.vercel.app/">co-match.vercel.app</ExternalLink>
+        </>
+      ),
     },
     {
       title: "Security",
-      description: "Learning low-level security",
+      description: "Learning low-level security: from 'simple' binary exploitation to kernel exploits and more",
       badge: "☠️",
       tags: [Tags.security, Tags.education, Tags.assembly],
       details: (
@@ -90,9 +115,10 @@ const work: Record<string, TProject[]> = {
       href: "https://github.com/orgs/noir-lang/discussions/8595",
       tags: [Tags.mpc, Tags.zk, Tags.webassembly, Tags.noir, Tags.zk_tls],
       startDate: new Date("2025-07-01"),
+      endDate: "present",
     },
     {
-      title: "noir recursive proofs",
+      title: "Noir recursive proofs",
       description: "Implementing recursive proofs in Noir",
       href: "https://github.com/teddav/noir-recursive",
       tags: [Tags.noir, Tags.zk],
@@ -107,21 +133,22 @@ const work: Record<string, TProject[]> = {
       startDate: new Date("2025-06-01"),
     },
     {
-      title: "stark_by_hand",
+      title: "stark_by_hand with Sage",
       description: "Implementing STARKs from scratch to understand the protocol deeply",
       href: "https://github.com/teddav/stark_by_hand",
       tags: [Tags.stark, Tags.zk, Tags.education, Tags.python],
       startDate: new Date("2025-03-15"),
     },
     {
-      title: "halo2 soundness bug",
-      description: "Found a soundness bug in PSE's Halo2 proving system",
+      title: "Halo2 lookup table soundness bug",
+      description: "Found a soundness bug in 'lookup_any' in PSE's Halo2 proving system",
       href: "https://github.com/privacy-scaling-explorations/halo2/issues/335",
       tags: [Tags.halo2, Tags.security, Tags.auditing],
       details: (
-        <b>
-          <a href="https://github.com/teddav/poc-underconstrained-halo2/tree/main">PoC for the bug</a>
-        </b>
+        <>
+          You&apos;ll find the PoC for the bug{" "}
+          <ExternalLink href="https://github.com/teddav/poc-underconstrained-halo2/tree/main">on this repo</ExternalLink>
+        </>
       ),
       startDate: new Date("2024-05-17"),
     },
@@ -131,12 +158,21 @@ const work: Record<string, TProject[]> = {
       href: "https://github.com/electisec/summa-audit-report",
       tags: [Tags.halo2, Tags.security, Tags.auditing, Tags.rust, Tags.evm, Tags.zk],
       details: (
-        <b>
-          <a href="https://pse.dev/projects/summa">Summa</a>
-          I also wrote a detailed code walkthrough of the solidity contracts, and the ZK circuits:
-          <br />
-          <Link href="/notes/summa-contracts">here</Link>
-        </b>
+        <>
+          <div>
+            More details on <ExternalLink href="https://pse.dev/projects/summa">Summa</ExternalLink>
+          </div>
+          <div>
+            I also wrote a detailed code walkthrough of the{" "}
+            <Link href="/notes/summa-contracts" className="text-blue-600 hover:text-blue-700 hover:underline">
+              solidity contracts
+            </Link>{" "}
+            and{" "}
+            <Link href="/notes/summa-circuits" className="text-blue-600 hover:text-blue-700 hover:underline">
+              the ZK circuits
+            </Link>
+          </div>
+        </>
       ),
       startDate: new Date("2024-04-20"),
     },
@@ -150,17 +186,19 @@ const work: Record<string, TProject[]> = {
     },
     {
       title: "halo2-starter",
-      description: "Battle-tested testing patterns for Halo2 development",
+      description: "Template to quickly start a Halo2 project",
       href: "https://github.com/teddav/halo2-starter",
       tags: [Tags.halo2, Tags.testing],
       startDate: new Date("2024-06-01"),
+      details: <>All you need to start a Halo2 project, from testing to production</>,
     },
     {
-      title: "halo2-soundness-bugs",
-      description: "Exploring edge cases and exploits in Halo2 implementations",
+      title: "Write secure Halo2 circuits",
+      description: "Exploring edge cases and exploits in Halo2 circuits",
       href: "https://github.com/teddav/halo2-soundness-bugs",
       tags: [Tags.halo2, Tags.security, Tags.research, Tags.education],
       startDate: new Date("2025-02-20"),
+      details: <>I wrote a detailed blog post, and multiple circuits, on how to write secure Halo2 circuits, and how to test them</>,
     },
     {
       title: "Halo2 cheating feature",
@@ -168,13 +206,29 @@ const work: Record<string, TProject[]> = {
       href: "https://github.com/privacy-scaling-explorations/halo2/pull/352",
       tags: [Tags.halo2, Tags.mock_prover, Tags.security],
       startDate: new Date("2024-06-10"),
+      details: (
+        <>
+          A nice feature I wanted while developing circuits.
+          <br />
+          This was my first PR in Halo2, it was fun to dive into the codebase
+        </>
+      ),
     },
     {
-      title: "halo2 tornado cash",
-      description: "Implementing tornado cash in Halo2",
+      title: "Tornado Cash with Halo2",
+      description: "Implementing Tornado Cash in Halo2",
       href: "https://teddav.github.io/blog/tornado-halo2",
       tags: [Tags.halo2, Tags.security, Tags.privacy],
       startDate: new Date("2024-02-01"),
+      details: (
+        <>
+          Fun project to get more familiar with both Tornado Cash and Halo2.
+          <br />
+          On top of the circuits, I wrote a detailed blog post to explain everything
+          <br />A part 2 is supposed to come someday... mostly to implement the Poseidon hash function (from scratch?), but I haven&apos;t
+          had the time to finish
+        </>
+      ),
     },
   ],
 
